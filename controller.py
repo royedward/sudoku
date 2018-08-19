c05d058a6217b01868a7cc182f38d122e072d615
Roy <royedwardanderson@gmail.com>
2018-08-19 19:27:23 -0400

    added shuffle

--- controller.py
+++ controller.py
@@ -18,7 +18,8 @@ class Controller:
 
         self.game_index = list(range(1, Controller.num_of_games))
 
-        shuffle(self.game_index)
+        for _ in range(0, 6):
+            shuffle(self.game_index)
 
     def popup_you_won(self):
         showinfo("Congratulations!", "You Won The Game - Play Another Game")
@@ -44,7 +45,8 @@ class Controller:
 
         if len(self.game_index) == 0:
             self.game_index = list(range(1, self.num_of_games))
-            for i in range(0, 6):
+
+            for _ in range(0, 6):
                 shuffle(self.game_index)
 
         game_num = str(self.game_index.pop())
